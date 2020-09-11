6796ac47a6a21f04bd74df31f18779661fc9d20d
imbyungjun <nt11294@nts-corp.com>
2020-09-11 15:46:36 +0900

    Change to update user information when authenticated

--- ngrinder-controller/src/main/java/org/ngrinder/security/DefaultLdapLoginPlugin.java
+++ ngrinder-controller/src/main/java/org/ngrinder/security/DefaultLdapLoginPlugin.java
@@ -47,7 +47,6 @@ public class DefaultLdapLoginPlugin implements OnLoginRunnable {
 		if (foundOne != null) {
 			user.setId(foundOne.getId());
 			user.setRole(foundOne.getRole());
-			userService.saveWithoutPasswordEncoding(user);
 		}
 
 		return user;
